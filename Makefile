abaa91978ea93df99ab0a534adda5de8edad5b98
carevilla <carevilla@miners.utep.edu>
2023-09-23 15:34:55 -0600

    Task 1 complete implemented time1.c

--- Makefile
+++ Makefile
@@ -138,6 +138,7 @@ UPROGS=\
 	$U/_pstest\
 	$U/_uptime\
 	$U/_time1\
+	$U/_matmul\
 
 fs.img: mkfs/mkfs README $(UPROGS)
 	mkfs/mkfs fs.img README $(UPROGS)
--- user/time1.c
+++ user/time1.c
@@ -7,17 +7,37 @@
 #include "kernel/types.h"
 #include "kernel/stat.h"
 #include "user/user.h"
+#include <stddef.h>
 
 int main(int argc, char **argv)
 {
-  int i;
+  int start_t, end_t;
+  char *argv_list[] = { (char *) NULL };  /* empty arg list */
 
-  if(argc < 2){
-    fprintf(2, "usage: time1 arguement\n");
+  /* must have one argument only */
+  if(argc != 2){
+    fprintf(2, "usage: time1 argument\n");
     exit(1);
   }
+
+  start_t = uptime();   /* start clock */
+  int pid = fork();     /* create child */
+
+  /* we are in the child process */
+  if ( pid == 0 ) {
+    exec(argv[1],argv_list);
+    exit(0);
+  }
   
-  for(i=1; i<argc; i++)
-    printf((argv[i]));
+  /* in parent but error occured in fork() */
+  else if ( pid == -1 ) {
+    exit(-1);
+  }
+
+  /* in parent waiting for child to finish, compute results */
+  wait(&pid);
+  end_t = uptime();
+  printf("elapsed time: %d ticks\n", (end_t - start_t));
+
   exit(0);
 }
