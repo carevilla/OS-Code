1428cb9d5db11c1d2d94f3c641e1eab24e23269d
carevilla <carevilla@miners.utep.edu>
2023-09-30 15:12:57 -0600

    added entry and wait2 systemcall info to kernal

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -102,15 +102,17 @@ extern uint64 sys_sbrk(void);
 extern uint64 sys_sleep(void);
 extern uint64 sys_unlink(void);
 extern uint64 sys_wait(void);
+extern uint64 sys_wait2(void);
 extern uint64 sys_write(void);
 extern uint64 sys_uptime(void);
 extern uint64 sys_getprocs(void);
-extern uint64 sys_wait2(void);
+
 
 static uint64 (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
 [SYS_exit]    sys_exit,
 [SYS_wait]    sys_wait,
+[SYS_wait2]   sys_wait2,
 [SYS_pipe]    sys_pipe,
 [SYS_read]    sys_read,
 [SYS_kill]    sys_kill,
@@ -130,7 +132,6 @@ static uint64 (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_getprocs]   sys_getprocs,
-[SYS_wait2]   sys_wait2,
 };
 
 void
