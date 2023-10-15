874d9aab0ca8a895db533b4945250973f9a743b8
carevilla <carevilla@miners.utep.edu>
2023-10-15 11:13:10 -0600

    added get and set priority to syscall.c

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -106,6 +106,8 @@ extern uint64 sys_wait2(void);
 extern uint64 sys_write(void);
 extern uint64 sys_uptime(void);
 extern uint64 sys_getprocs(void);
+extern uint64 sys_getpriority(void);
+extern uint64 sys_setpriority(void);
 
 
 static uint64 (*syscalls[])(void) = {
@@ -132,6 +134,8 @@ static uint64 (*syscalls[])(void) = {
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
 [SYS_getprocs]   sys_getprocs,
+[SYS_getpriority] sys_getpriority,
+[SYS_setpriority] sys_setpriority
 };
 
 void
